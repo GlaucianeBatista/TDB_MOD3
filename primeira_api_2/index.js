9e3786f102979fc089222c8e00feb1b24b2cc981
Glauciane Batista <glaucianegnomo@gmail.com>
2025-12-03 22:01:25 -0300

    Set up express server and add movie data endpoint

--- primeira_api_2/index.js
+++ primeira_api_2/index.js
@@ -1,11 +1,37 @@
 //_____________( Cria uma constante para usar a biblioteca express e ver todos os itens )_______________________
 const express = require ("express");
-
 //_____________( Cria uma constante com o nome app, para usar a biblioteca express )____________________________
-const app = express();
-
+const locadora = express();
 //_____________( Cria arrow function para acessar a porta 5500 e mostrar a frase da porta )______________________
-app.listen(5500, () => {
+locadora.listen(5500, () => {
 	console.log("O SERVIDOR ESTÁ RODANDONA PORTA:5500");
+});
+//____________________________________________________//Filmes da Locadora//____________________________________________________\\
+const locadora_de_filme = [
+		{
+			id: "01", 
+			nome: "O Espetacular Homem-Aranha", 
+			gênero: "super-heroi", 
+			classificação: "+12", 
+			valor: "R$06,09"
+		},
+		{
+			id: "02", 
+			nome: "Homem-Aranha: Através do Aranhaverso ", 
+			gênero: "super-heroi", 
+			classificação: "+10", 
+			valor: "R$22,80"
+		},
+		{
+			id: "03",
+			nome: "Alien, o Oitavo Passageiro", 
+			gênero: "ficção cientifica", 
+			classificação: "+14", 
+			valor: "R$25,99"
+		}
+	];
+//______________( consultar um endereço de site chamado pessoas, que mostra a palavra capivara )_____________________________________________________________________
+locadora.get("/locadora_de_filme", (request, response) => {
+	response.status(200).send(locadora_de_filme);
 });
 
