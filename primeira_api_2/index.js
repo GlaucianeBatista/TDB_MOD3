e83a7501993125e6cbf277062492356b2bf1d21f
Glauciane Batista <glaucianegnomo@gmail.com>
2025-12-08 21:59:31 -0300

    Refactor index.js to improve API functionality

--- primeira_api_2/index.js
+++ primeira_api_2/index.js
@@ -1,37 +1,65 @@
+
 //_____________( Cria uma constante para usar a biblioteca express e ver todos os itens )_______________________
-const express = require ("express");
+const express = require("express");
 //_____________( Cria uma constante com o nome app, para usar a biblioteca express )____________________________
-const locadora = express();
-//_____________( Cria arrow function para acessar a porta 5500 e mostrar a frase da porta )______________________
-locadora.listen(5500, () => {
+const app = express();
+//_____________( Converse a sintaxe de JavaScript do VScode para o Json do insomnia para NodeJs )_______________
+app.use(express.json());
+
+app.listen(5500, () => {
 	console.log("O SERVIDOR ESTÁ RODANDONA PORTA:5500");
 });
-//____________________________________________________//Filmes da Locadora//____________________________________________________\\
-const locadora_de_filme = [
-		{
-			id: "01", 
-			nome: "O Espetacular Homem-Aranha", 
-			gênero: "super-heroi", 
-			classificação: "+12", 
-			valor: "R$06,09"
-		},
-		{
-			id: "02", 
-			nome: "Homem-Aranha: Através do Aranhaverso ", 
-			gênero: "super-heroi", 
-			classificação: "+10", 
-			valor: "R$22,80"
-		},
-		{
-			id: "03",
-			nome: "Alien, o Oitavo Passageiro", 
-			gênero: "ficção cientifica", 
-			classificação: "+14", 
-			valor: "R$25,99"
-		}
-	];
-//______________( consultar um endereço de site chamado pessoas, que mostra a palavra capivara )_____________________________________________________________________
-locadora.get("/locadora_de_filme", (request, response) => {
-	response.status(200).send(locadora_de_filme);
+
+const pessoas = [{
+	nome: "Glauci",
+	email: "glaucianegnomo@gmail.com",
+	status: true
+}];
+
+app.post("/cadastrar", (request, response) => {
+	//const nome = request.body.nome;
+	//const email = request.body.email;
+	//const status = request.body.status;
+	const data = request.body;
+
+	console.log(data);
+
+	data.forEach(i => {
+		const { nome, email, status } = i;
+		pessoas.push({ nome, email, status });
+	});
+	// const { nome, email, status} = request.body;
+
+
+	// pessoas.push({nome,email,status});
+
+	//return response.send('');
+	//return response.send("Olá, tudo bem? Em que posso ajudar?");
+	// return response.send({ nome, email, status });
+	return response.send(data);
+
+
 });
+
+app.get("/consultar", (request, response) => {
+
+	const { nome } = request.query;
+
+	if (nome) {
+		return response.send(pessoas.filter((i) => {
+			return i.nome === nome;
+		}));
+	}
+
+	return response.send(pessoas);
+
+});
+
+//_____________( Cria arrow function para acessar a porta 5500 e mostrar a frase da porta )______________________
+
+//http://127.0.0.1:5500/ --> Cannot Get/
+
+
+
+
 
